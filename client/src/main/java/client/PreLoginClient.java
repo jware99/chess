d8d65c13916e72d65f02fd83271cd80e48775d48
joshware1 <joshware99@gmail.com>
2025-03-24 16:54:49 -0600

    implemented some of the prelogin repl

--- client/src/main/java/client/PreLoginClient.java
+++ client/src/main/java/client/PreLoginClient.java
@@ -1,4 +1,14 @@
 package client;
 
+import facade.ServerFacade;
+
 public class PreLoginClient {
+
+    private final ServerFacade facade;
+    private final String serverUrl;
+
+    public PreLoginClient(String serverUrl) {
+        facade = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
 }
--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -1,4 +1,48 @@
 package client;
 
+import client.websocket.NotificationHandler;
+import facade.ServerFacade;
+import webSocketMessages.Notification;
+
+import java.util.Scanner;
+
+import static client.EscapeSequences.*;
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.Constants.RESET;
+import static java.awt.Color.GREEN;
+import static java.awt.Color.RED;
+
 public class Repl {
+    private final PreLoginClient preLoginClient;
+    private final ServerFacade facade;
+
+    public Repl(String serverUrl) {
+        preLoginClient = new PreLoginClient(serverUrl);
+        this.facade = new ServerFacade(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to 240 chess. Type Help to get started.");
+        System.out.print(preLoginClient.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = preLoginClient.eval(line);
+                System.out.print(RED + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET + ">>> " + GREEN);
+    }
+
 }
