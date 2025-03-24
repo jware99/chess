d2b5eeb49924a8393f48ca177fd85697151a1d2e
joshware1 <joshware99@gmail.com>
2025-03-24 14:04:50 -0600

    wrote login and logout tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -3,14 +3,16 @@ package client;
 import exception.ResponseException;
 import org.junit.jupiter.api.*;
 import request.LoginRequest;
+import request.LogoutRequest;
 import request.RegisterRequest;
+import result.LoginResult;
+import result.LogoutResult;
 import result.RegisterResult;
 import server.Server;
 import facade.ServerFacade;
 import service.ErrorException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
@@ -55,7 +57,6 @@ public class ServerFacadeTests {
         assertNotNull(registerResult);
         assertNotNull(registerResult.authToken());
         assertEquals("username", registerResult.username());
-
     }
 
     @Test
@@ -67,10 +68,38 @@ public class ServerFacadeTests {
     @Test
     public void loginSuccess() throws ResponseException {
         LoginRequest loginRequest = new LoginRequest("myUsername", "myPassword");
+        LoginResult loginResult = facade.loginResult(loginRequest);
 
-        assertNotNull(registerResult);
-        assertNotNull(registerResult.authToken());
-        assertEquals("username", registerResult.username());
+        assertNotNull(loginResult);
+        assertNotNull(loginResult.authToken());
+        assertEquals("username", loginResult.username());
+    }
+
+    @Test
+    public void loginFail() throws ResponseException {
+        LoginRequest loginRequest = new LoginRequest("username", "password");
+        Assertions.assertThrows(ErrorException.class, () -> facade.loginResult(loginRequest));
+    }
+
+    @Test
+    public void logoutSuccess() throws ResponseException {
+        LoginRequest loginRequest = new LoginRequest("myUsername", "myPassword");
+        LoginResult loginResult = facade.loginResult(loginRequest);
+
+        LogoutRequest logoutRequest = new LogoutRequest(loginResult.authToken());
+        LogoutResult logoutResult = facade.logoutResult(logoutRequest);
+
+        assertNull(logoutResult);
+    }
+
+    @Test
+    public void logoutFail() throws ResponseException {
+        LoginRequest loginRequest = new LoginRequest("myUsername", "myPassword");
+        facade.loginResult(loginRequest);
+
+        LogoutRequest logoutRequest = new LogoutRequest("wrong token");
+
+        Assertions.assertThrows(ErrorException.class, () -> facade.logoutResult(logoutRequest));
 
     }
 
