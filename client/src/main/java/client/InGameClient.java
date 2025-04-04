ec27afbadd1c550df0e0ecb59ae35d5e8d05c2d8
joshware1 <joshware99@gmail.com>
2025-04-04 13:05:23 -0600

    set up the ingameclient, just need to implement the functions

--- client/src/main/java/client/InGameClient.java
+++ client/src/main/java/client/InGameClient.java
@@ -1,18 +1,25 @@
 package client;
 
 import facade.ServerFacade;
+import websocket.WebSocketFacade;
+import websocket.messages.ServerMessage;
+
 import java.util.Arrays;
 
 public class InGameClient {
 
     private  String authToken;
     private  State state;
+    private ServerFacade facade;
+    private WebSocketFacade ws;
+    private ServerMessage message;
 
 
-    public InGameClient(String serverUrl, State state, String authToken) {
+    public InGameClient(String serverUrl, State state, String authToken, ServerMessage message) {
         ServerFacade facade = new ServerFacade(serverUrl);
         this.state = state;
         this.authToken = authToken;
+        this.message = message;
     }
 
     public String eval(String authToken, State state,  String input) {
@@ -21,14 +28,20 @@ public class InGameClient {
         var cmd = (tokens.length > 0) ? tokens[0] : "help";
         var params = Arrays.copyOfRange(tokens, 1, tokens.length);
         return switch (cmd) {
-            case "quit" -> "quit";
+            case "highlight" -> highlight();
+            case "redraw" -> redraw();
+            case "resign" -> resign();
+            case "leave" -> "quit";
             default -> help();
         };
     }
 
     public static String help() {
         return """
-                quit - playing chess
+                highlight - possible moves
+                redraw - chess board
+                resign - current game
+                leave - current game
                 help - with possible commands
                 """;
     }
